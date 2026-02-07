e1e6ffc847312aac8840b8b27f331b76a3507bae
AlexMG-05 <a.martingz05@gmail.com>
2026-02-07 21:21:23 +0100

    feat: define BankAccount class interface

---
+++ financial_systems/bank_account/Account.h
@@ -0,0 +1,25 @@
+#ifndef ACCOUNT_H //So file is read only once
+#define ACCOUNT_H
+#include <string>
+
+//CLASS DECLARATION
+class BankAccount{
+
+    private:
+    std::string account_holder;
+    std::string account_number;
+    double balance;
+
+    public:
+    //Constructor
+    BankAccount(std::string name, std::string acc_num, double initial_deposit);
+
+    void deposit(double amount);
+    bool withdraw(double amount); //T = successful, F = insufficient funds
+
+    //Read-only
+    double get_balance() const;
+    double display_info() const;
+};
+
+#endif // ACCOUNT_H
